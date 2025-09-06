f46675d4601837c31322ff75c6d24c14de8e3a1e
AntonZakharo <antonchik2019top@gmail.com>
2025-09-06 21:57:04 +0300

    app.vue

---
+++ src/components/BaseColumn.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import TaskItem from './TaskItem.vue'
+</script>
+
+<template>
+  <div class="main__column">
+    <div class="column__title">
+      <p>Без статуса</p>
+    </div>
+    <div class="cards">
+      <TaskItem />
+    </div>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,28 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import BaseHeader from '@/components/BaseHeader.vue'
+import LogoutPopUp from '@/components/LogoutPopUp.vue'
+import NewTaskPopUp from '@/components/NewTaskPopUp.vue'
+import TaskDesk from '@/components/TaskDesk.vue'
+import TaskMenu from '@/components/TaskMenu.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="wrapper">
+    <!-- pop-up start-->
+
+    <LogoutPopUp />
+
+    <NewTaskPopUp />
+
+    <TaskMenu />
+
+    <!-- pop-up end-->
+
+    <BaseHeader></BaseHeader>
+    <main class="main">
+      <div class="container">
+        <TaskDesk></TaskDesk>
+      </div>
+    </main>
+  </div>
 </template>
