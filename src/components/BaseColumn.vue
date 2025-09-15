ee32c79bb41a7fa33ee17056df88716c07a31201
AntonZakharo <antonchik2019top@gmail.com>
2025-09-15 22:42:48 +0300

    taskdesk

--- src/components/BaseColumn.vue
+++ src/components/BaseColumn.vue
@@ -1,14 +1,39 @@
-<script setup lang="ts">
-import TaskItem from './TaskItem.vue'
-</script>
-
 <template>
   <div class="main__column">
     <div class="column__title">
-      <p>Без статуса</p>
+      <p ref="parag">{{ status }}</p>
     </div>
-    <div class="cards">
-      <TaskItem />
-    </div>
+    <div class="cards"></div>
   </div>
 </template>
+
+<script setup>
+import TaskItem from './TaskItem.vue'
+import { testTasks, columns } from '@/mocks/tasks'
+defineProps({
+  status: String,
+})
+
+import { onMounted, ref, render, h } from 'vue'
+const parag = ref(null)
+
+onMounted(() => {
+  for (let i = 0; i < testTasks.length; i++) {
+    const status = parag.value.innerHTML
+    if (testTasks[i].status === status) {
+      const cards = document.querySelectorAll('.cards')[columns.indexOf(status)]
+
+      const vnode = h(TaskItem, {
+        topic: testTasks[i].topic,
+        title: testTasks[i].title,
+        date: testTasks[i].date,
+      })
+
+      const container = document.createElement('div')
+      cards.appendChild(container)
+
+      render(vnode, container)
+    }
+  }
+})
+</script>
