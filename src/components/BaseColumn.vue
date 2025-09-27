858dc925686eaa46fbf32533f6ba3b6a47f0bc5d
AntonZakharo <antonchik2019top@gmail.com>
2025-09-27 14:46:07 +0300

    добавление карточек через v-for

--- src/components/BaseColumn.vue
+++ src/components/BaseColumn.vue
@@ -1,41 +1,25 @@
 <template>
   <div class="main__column">
     <div class="column__title">
-      <p ref="parag">{{ status }}</p>
+      <p>{{ status }}</p>
     </div>
-    <div class="cards"></div>
+    <div class="cards">
+      <TaskItem
+        v-for="(task, index) in testTasks.filter((t) => t.status === status)"
+        :key="index"
+        :topic="task.topic"
+        :title="task.title"
+        :date="task.date"
+      />
+    </div>
   </div>
 </template>
 
 <script setup>
 import TaskItem from './TaskItem.vue'
-import { testTasks, columns } from '@/mocks/tasks'
+import { testTasks } from '@/mocks/tasks'
 defineProps({
   status: String,
-})
-
-import { onMounted, ref, render, h } from 'vue'
-const parag = ref(null)
-
-onMounted(() => {
-  for (let i = 0; i < testTasks.length; i++) {
-    const status = parag.value.innerHTML
-
-    if (testTasks[i].status === status) {
-      const cards = document.querySelectorAll('.cards')[columns.indexOf(status)]
-
-      const vnode = h(TaskItem, {
-        topic: testTasks[i].topic,
-        title: testTasks[i].title,
-        date: testTasks[i].date,
-      })
-
-      const container = document.createElement('div')
-      cards.appendChild(container)
-
-      render(vnode, container)
-    }
-  }
 })
 </script>
 
